d47e9a94658828131fd202ac775b28494ae8e3ce
Lucas Slebos <lucas@slebos.nz>
2024-04-19 17:32:55 +1200

    fix vimplugin remove prefix

--- lua/plugins/lazy-nix-helper.lua
+++ lua/plugins/lazy-nix-helper.lua
@@ -1,5 +1,5 @@
 return {
     "b-src/lazy-nix-helper.nvim", tag = "v0.4.0",
     name = "lazy-nix-helper.nvim",
-    dir = require("lazy-nix-helper").get_plugin_path("vimplugin-lazy-nix-helper.nvim")
+    dir = require("lazy-nix-helper").get_plugin_path("lazy-nix-helper.nvim")
 }

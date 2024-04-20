cda14560a340ab373c57c4f03ae376e0df96bb70
Lucas Slebos <lucas@slebos.nz>
2024-04-20 16:07:18 +1200

    add stylua and pre-commit

--- flake.nix
+++ flake.nix
@@ -5,7 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{self, nixpkgs, ...}: {
+  outputs = inputs@{self, nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+  in {
     homeManagerModules.neovim = import ./nix;
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        stylua
+        pre-commit
+      ];
+    };
   };
 }
--- init.lua
+++ init.lua
@@ -8,14 +8,14 @@ else
 end
 
 if not vim.loop.fs_stat(lazy_nix_helper_path) then
-vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/b-src/lazy-nix-helper.nvim.git",
-    "--branch=v0.4.0",
-    lazy_nix_helper_path,
-})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/b-src/lazy-nix-helper.nvim.git",
+        "--branch=v0.4.0",
+        lazy_nix_helper_path,
+    })
 end
 
 -- add the Lazy Nix Helper plugin to the vim runtime
@@ -42,14 +42,14 @@ end
 -- set lazypath variable to the path where the Lazy plugin will be installed on your system
 -- if Lazy is not already installed there, download and install it
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 -- add the Lazy plugin to the vim runtime
 vim.opt.rtp:prepend(lazypath)
